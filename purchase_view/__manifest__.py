dcec48e6605041193f761d0fb22b99e1d72c7d85
Dxpro1 <121645690+Dxpro1@users.noreply.github.com>
2024-08-16 10:35:32 +0800

    Add files via upload

---
+++ purchase_view/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name' : 'employees',
+    'description' : 'wala lang',
+    'summary' : '',
+    'depends': ['base', 'hr', 'purchase'],  # Corrected: list of strings    
+    'data' : [
+        'views/purchase_view.xml',
+    ],
+}
---
+++ purchase_view/models/purchase_order.py
@@ -0,0 +1,7 @@
+from odoo import models, fields, api
+
+class PurchaseOrder(models.Model):
+    _inherit = 'purchase.order'   
+    # num_1 = fields.Interger()
+    # num_2 = fields.Interger()
+    # num_3 = fields.Interger()
